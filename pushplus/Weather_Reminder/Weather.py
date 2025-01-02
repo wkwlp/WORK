48a2ff231a259edfbb553b42c36079a0362d5a4f
wangke <1821647520@qq.com>
2025-01-02 14:49:24 +0800

    优化代码结构

--- pushplus/Weather_Reminder/Weather.py
+++ pushplus/Weather_Reminder/Weather.py
@@ -2,6 +2,7 @@ import requests
 import os
 from datetime import datetime, timedelta
 import logging
+from pushplus.common import *
 
 # 配置日志记录
 logging.basicConfig(
@@ -256,50 +257,6 @@ class WeatherInfoFetcher:
         return advices.get(weather_condition, '明日天气温馨提示：亲爱的老婆，高德地图返回的内容不在代码范围内！')
 
 
-class WeatherReminderSender:
-    """
-    通过PushPlus服务发送邮件提醒的类。
-
-    Attributes:
-        pushplus_token (str): PushPlus的服务Token。
-    """
-    logger = logging.getLogger(__name__)  # 创建一个与当前模块同名的日志记录器
-
-    def __init__(self):
-        """
-        初始化WeatherReminderSender实例，从环境变量中读取PushPlus的服务Token。
-        """
-        self.pushplus_token = os.environ.get('PUSHPLUS_TOKEN')
-        self.logger.info("SendEmail 初始化完成")
-
-    def send_reminder_email(self, title, content):
-        """
-        通过PushPlus服务发送邮件提醒。
-
-        Args:
-            title (str): 邮件标题。
-            content (str): 邮件内容。
-        """
-        url = "http://www.pushplus.plus/send"
-        # 构建发送邮件所需的数据字典
-        data = {
-            "token": self.pushplus_token,  # 推送使用的Token
-            "title": title,  # 邮件标题
-            "content": content,  # 邮件内容
-            "topic": "wkwlp",  # 群组编码
-            "template": "txt",  # 使用的邮件模板，此处使用纯文本格式
-            "channel": "mail"  # 指定推送方式为邮件
-        }
-        # 设置请求头，告知服务器我们将发送JSON格式的数据
-        headers = {'Content-Type': 'application/json'}
-        # 使用POST方法发送数据，并获取响应对象
-        response = requests.post(url, json=data, headers=headers)
-        # 判断请求是否成功
-        if response.status_code == 200:
-            self.logger.info("邮件提醒发送成功")
-        else:
-            self.logger.error(f"邮件提醒发送失败，状态码：{response.status_code}")
-
 
 def main():
     """
@@ -310,7 +267,7 @@ def main():
     # 创建天气信息获取器实例
     weather_fetcher = WeatherInfoFetcher()
     # 创建邮件发送器实例
-    email_sender = WeatherReminderSender()
+    email_sender = SendEmail()
 
     # 获取实时天气信息
     realtime_weather = weather_fetcher.fetch_live_weather_info()
@@ -327,7 +284,7 @@ def main():
     weather = f'{realtime_weather}{forecast_weather}{weather_condition}'
     logger.info(f"完整天气信息：{weather}")
     # 发送邮件提醒
-    email_sender.send_reminder_email('天气提醒', weather)
+    email_sender.send_reminder_email('天气提醒', weather, is_group_send=False)
 
 
 if __name__ == "__main__":
